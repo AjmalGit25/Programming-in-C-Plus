401367c9273a09635620b282fab16489d216f8cc
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-04-16 04:44:07 +0530

    Code added & improved

---
+++ Square root - sqrt().cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main () {
+    int c = 365;
+    long long b = sqrt (c);
+    
+    cout << "Square root of " << c << " = " << b;
+
+    return 0;
+}

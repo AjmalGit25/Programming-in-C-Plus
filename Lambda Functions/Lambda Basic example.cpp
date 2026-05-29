3f58336bfae064d6c2cc8d99ff1528d439695da5
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-05-29 23:58:46 +0530

    bug fixed

--- Lambda Functions/Lambda Basic example.cpp
+++ Lambda Functions/Lambda Basic example.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 int main () {
 	auto message = []() {
@@ -10,3 +11,17 @@ int main () {
 	return 0;
 }
 
+
+/*
+
+Lambda Functions:
+	-> A lambda function is a small, anonymous function you can write directly in your code. 
+	-> It's useful when you need a quick function without naming it or declaring it separately.
+	-> Think of it as a "mini function on the fly."
+
+----------------------------------------------------------
+Syntax:
+	[capture] (parameters) { code };
+
+
+*/
